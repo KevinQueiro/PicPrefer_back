9f7ce81b1ffe4681b0ce910620b5cb736bb0875b
KevinQueiro <73044295+KevinQueiro@users.noreply.github.com>
2023-08-24 22:12:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { connectToDB } from './utils/mongoose.js'
 
 async function main() {
   try {
+  const port = process.env['PORT']
   await connectToDB();
   app.listen(port)
   console.log(`server on port ${port}`)  
