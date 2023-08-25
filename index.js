6026ee020a17dc3bfb2a3622521efa55391705ab
KevinQueiro <73044295+KevinQueiro@users.noreply.github.com>
2023-08-24 21:58:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import app from './app.js'
 import { connectToDB } from './utils/mongoose.js'
 
 async function main() {
+  try {
   await connectToDB();
   app.listen(4000)
-  console.log('server on port 4000')
+  console.log('server on port 4000')  
+  } catch (error){
+    console.log(error)
+  }
 }
 
 main()
